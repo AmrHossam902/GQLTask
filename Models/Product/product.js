c52d3b9f6eb7b882fcd012b3c669a7ddb5237b47
Amr <amrhossam902@gmail.com>
2021-09-11 04:13:44 +0200

    database models added

---
+++ Models/Product/product.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-multi-str */
+
+/**
+ * Interface for creating Db interface that handles products queries only
+ * @param {{runQuery:Function, close:Function}} dbInterface
+ * object that has direct interaction with the database,
+ * returns an object containg some functions to interact with the database
+ */
+function ProductsModel(dbInterface) {
+  this.db = dbInterface;
+}
+
+/**
+ * function that adds a new product in the database
+ * @param {string} name name of the product
+ * @param {number} price email of the product
+ * @param {number} userId Id of the user who owns the product
+ * @return {Promise<{insertId:number}>| Promise<{type:string, msg:string}>}
+ * returns {insertId} incase of success,
+ * returns {type, msg} in case of failure
+ */
+ProductsModel.prototype.addNewProduct = function(name, price, userId) {
+  const query = 'insert into store.products(name, price, userId) values\
+      (?, ?, ?);';
+  return this.db.runQuery(query, [name, price, userId]);
+};
+
+
+/**
+ * get all products of a specific user
+ * @param {number} userId id of the user to search for his/ her products
+ * @return {Promise<Array<{id: number, name:string
+ *             , price:number, iserId:number}>>|
+ *         Promise<{type:string, msg:string}>}
+ * returns an array of products filled or empty in case user has no products,
+ * {type, msg} in case of error
+ */
+ProductsModel.prototype.getProducts = function(userId) {
+  const query = 'select * from products where userId = ? order by price';
+  return this.db.runQuery(query, [userId])
+      .then( (result)=> {
+        return result;
+      });
+};
+
+
+/**
+ * get all products of all users
+ * @return {Promise<Array<{id: number, name:string
+ *             , price:number, iserId:number}>>|
+ *         Promise<{type:string, msg:string}>}
+ * returns an array of products filled or empty in case user has no products,
+ * {type, msg} in case of error
+ */
+ProductsModel.prototype.getAllProducts = function() {
+  const query = 'select * from products';
+  return this.db.runQuery(query, [])
+      .then( (result)=> {
+        return result;
+      });
+};
+
--- Models/dbInterface.js
+++ Models/dbInterface.js
@@ -6,52 +6,83 @@ const {
   dbPassword,
   dbName,
   host,
-} = JSON.parse(fs.readFileSync('./keys/keys.json').toString('utf-8'));
+} = JSON.parse(fs.readFileSync(__dirname + '/dbConfig.json').toString('utf-8'));
+
+/**
+ * Interface intended for handling Db connections
+ */
+function MysqlInterface() {}
 
 
-function MySqlInterface() {}
-
-MySqlInterface.createInterface = function() {
-  if (MySqlInterface.pool) {
-    return MySqlInterface.pool;
+/**
+ * static function intends to create a single pool and handle the pool
+ * whenever it's asked to
+ * @return {Object} pool object
+ */
+MysqlInterface.getPool = function() {
+  if (MysqlInterface.pool) {
+    return MysqlInterface.pool.getConnection;
   }
-  return MySqlInterface.pool = mysql.createPool({
+  MysqlInterface.pool = mysql.createPool({
     connectionLimit: 5,
     host,
     user: dbUser,
     password: dbPassword,
     database: dbName,
   });
+  return MysqlInterface.pool;
 };
 
-MySqlInterface.promisifiedQuery = function(queryString, params){
-    return new Promise(function(resolve, reject){
-        
-        pool.getConnection( function(err, connection){
-            if(err){
-                reject(err);
-                return;
+/**
+ * static function for creating an interface object
+ * which is responsible for handling queries
+ * @return {{runQuery:Function, close:Function}} run query returns a promise
+ */
+MysqlInterface.createInterface = function() {
+  return {
+    /**
+     * function that runs a query on the database server
+     * @param {string} queryString parameterized string for rhe query
+     * like "insert into users(name, mail) values(?, ?)"
+     * @param {Array} params array containing parameters to be provided
+     *  to the query string
+     * @return {{type:string, msg:string} | Object}
+     * {type, msg } in case of error
+     */
+    runQuery: function(queryString, params) {
+      return new Promise(function(resolve, reject) {
+        MysqlInterface.getPool().getConnection( function(err, connection) {
+          if (err) {
+            reject(err);
+            return;
+          }
+          connection.query(queryString, params, function(err, results) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(results);
             }
-            connection.query(queryString, params, function(err, results){
-                if(err)
-                    reject(err);
-                else
-                    resolve(results);
-                connection.release();
-            });
-
+            connection.release();
+          });
         });
-    })
-    .catch( (reason)=>{
-        if(reason.errno == -111){
-            return {
-                type : 'SERVER_ERROR',
-                msg : 'connection problem with mysql server'
-            };
+      }).catch( (reason)=>{
+        if (reason.errno == -111) {
+          return {
+            type: 'SERVER_ERROR',
+            msg: 'connection problem with mysql server',
+          };
         }
         return {
-            type : reason.code,
-            msg : reason.message
+          type: reason.code,
+          msg: reason.message,
         };
-   });
+      });
+    },
+    close: function() {
+      MysqlInterface.pool.close();
+    },
+  };
 };
+
+
+module.exports.createInterface = MysqlInterface.createInterface;
