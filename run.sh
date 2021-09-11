25b58234427f701d123a84d23522849b771cd9dc
Amr <amrhossam902@gmail.com>
2021-09-11 20:37:31 +0200

    readme file added

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-sudo rm ./* -r;
 git clone --branch master https://github.com/AmrHossam902/GQLTask.git GraphApp;
 git clone --branch Mysql https://github.com/AmrHossam902/GQLTask.git Mysql;
 
-mkdir ./Mysql/data
-docker-compose -f GraphApp/docker-compose.yml up;
+mkdir ../Mysql/data
+docker-compose up;
