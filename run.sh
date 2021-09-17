5ef15c939768ae1a5404d0ac7a01f2d805322382
Amr Hossam <amrhossam902@gmail.com>
2021-09-17 15:38:49 +0200

    Updating run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 git clone --branch master https://github.com/AmrHossam902/GQLTask.git GraphApp;
 git clone --branch Mysql https://github.com/AmrHossam902/GQLTask.git Mysql;
 
-mkdir ../Mysql/data
 docker-compose up;
